c69123d8e8845d0f5df3e4102eb9be6a0697a4b3
Felix.Khodakovskiy <felix.khodakovskiy@nexign.com>
2020-04-14 13:22:24 +1000

    add process

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,37 +15,134 @@
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import { 
+    tap,
+    pipe,
+    gt,
+    lt,
+    length,
+    has,
+    __,
+    allPass,
+    match,
+    replace,
+    pipeWith,
+    applySpec,
+    prop,
+    partialRight,
+    mathMod,
+    curry,
+    cond,
+    isNil,
+    not,
+    ifElse
+} from 'ramda';
 
 const api = new Api();
 
 /**
  * Я – пример, удали меня
  */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+// const wait = time => new Promise(resolve => {
+//     setTimeout(resolve, time);
+// })
+
+const validateValue = (value) => {
+    const checkLength = pipe(
+        length,
+        allPass([gt(__, 2), lt(__, 10)])
+    );
+
+    const checkNumber = pipe(
+        match(/^[0-9]+\.?[0-9]+$/g),
+        length,
+        gt(__, 0)
+    );
+
+    const checkPositive = pipe(
+        Number,
+        gt(__, 0)
+    );
+
+    return pipe(
+        replace(',', ''),
+        allPass([checkLength, checkNumber, checkPositive])
+    )(value);
+}
+
+const toNumber = (value) => {
+    return pipe(
+        replace(',', '.'),
+        Number,
+        Math.round
+    )(value);
+}
+
+const double = partialRight(Math.pow, [2]);
+
+const convertNum = api.get('https://api.tech/numbers/base');
+const getAnimal = (id) => api.get(`https://animals.tech/${id}`, {});
+
 
 const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+    if (!validateValue(value)) {
+        handleError('ValidationError');
+        return;
+    }
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+    const constructRequestParams = applySpec({
+        from: () => 10,
+        to: () => 2,
+        number: (x) => x
+      });
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+    const asyncPipe = pipeWith(
+        (fn, res) => (res && res.then) ?
+            res.then(fn)
+               .catch((err) => {handleError(err)})
+            : fn(res)
+    );
+    
+    const convertToBinary = pipe(
+        tap(writeLog),
+        constructRequestParams,
+        convertNum
+    );
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    const convertNumForAnimal = pipe(
+        tap(writeLog),
+        length,
+        tap(writeLog),
+        double,
+        tap(writeLog),
+        mathMod(__, 3),
+        tap(writeLog),
+    );
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+    asyncPipe([
+        toNumber,
+        convertToBinary,
+        ifElse(
+            has('result'),
+            asyncPipe([
+                prop('result'),
+                convertNumForAnimal,
+                getAnimal,
+                ifElse(
+                    has('result'),
+                    pipe(
+                        prop('result'),
+                        tap(writeLog),
+                        handleSuccess
+                    ),
+                    handleError
+                )
+            ]),
+            handleError
+        )
+    ])(value);
+
+    
 }
 
 export default processSequence;
